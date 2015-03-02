555b6c4aa8a62186d560a7ebf8e54fe2f39dbefb
Michael Zekoff <zekoff@gmail.com>
2015-03-02 04:41:07 +0000

    Constrain movement to grid.
    Sort of. This implementation sucks.

--- src/state/main.js
+++ src/state/main.js
@@ -5,6 +5,9 @@ define(['phaser', 'const', 'config'], function(Phaser, Const, Config) {
     var pickupsLayer;
     var cursors;
     var speed = Config.playerSpeed;
+    var currentTile;
+    var direction = Phaser.DOWN;
+    var threshold = 3;
     state.create = function() {
         state.game.stage.backgroundColor = 0xFFFFFF;
         var map = state.add.tilemap('test');
@@ -15,17 +18,69 @@ define(['phaser', 'const', 'config'], function(Phaser, Const, Config) {
         player = state.add.sprite(Const.TILE_SIZE * 1.5,
             Const.TILE_SIZE * 1.5, 'badman');
         state.physics.arcade.enable(player);
-        player.anchor.set(0.5);
+        player.anchor.set(0.5); // removing this causes weird placement...
 
         cursors = state.input.keyboard.createCursorKeys();
+        currentTile = new Phaser.Point();
     };
     state.update = function() {
-        if (cursors.left.isDown) player.body.velocity.x = -speed;
-        else if (cursors.right.isDown) player.body.velocity.x = speed;
-        else if (cursors.up.isDown) player.body.velocity.y = -speed;
-        else if (cursors.down.isDown) player.body.velocity.y = speed;
-        else player.body.velocity.set(0);
+        // allow reversing direction any time
+        if (cursors.up.isDown && direction === Phaser.DOWN) direction = Phaser.UP;
+        if (cursors.down.isDown && direction === Phaser.UP) direction = Phaser.DOWN;
+        if (cursors.left.isDown && direction === Phaser.RIGHT) direction = Phaser.LEFT;
+        if (cursors.right.isDown && direction === Phaser.LEFT) direction = Phaser.RIGHT;
+        // allow turning if at intersection...
+        if (state.math.fuzzyEqual(player.x, currentTile.x + Const.TILE_SIZE / 2, threshold) &&
+            state.math.fuzzyEqual(player.y, currentTile.y + Const.TILE_SIZE / 2, threshold)) {
+            // ...and target tile is open
+            // this is just a horrible proof-of-concept
+            var newDirection = null;
+            if (cursors.up.isDown) newDirection = Phaser.UP;
+            if (cursors.down.isDown) newDirection = Phaser.DOWN;
+            if (cursors.left.isDown) newDirection = Phaser.LEFT;
+            if (cursors.right.isDown) newDirection = Phaser.RIGHT;
+            // if player turned, rectify position to currentTile
+            var turned = false;
+            if (direction === Phaser.UP && (newDirection === Phaser.LEFT || newDirection === Phaser.RIGHT)) turned = true;
+            if (direction === Phaser.DOWN && (newDirection === Phaser.LEFT || newDirection === Phaser.RIGHT)) turned = true;
+            if (direction === Phaser.LEFT && (newDirection === Phaser.UP || newDirection === Phaser.DOWN)) turned = true;
+            if (direction === Phaser.RIGHT && (newDirection === Phaser.UP || newDirection === Phaser.DOWN)) turned = true;
+            if (turned) {
+                player.x = currentTile.x + Const.TILE_SIZE / 2;
+                player.y = currentTile.y + Const.TILE_SIZE / 2;
+            }
+            if (newDirection)
+                direction = newDirection;
+        }
+        // move player in correct direction
+        player.body.velocity.set(0);
+        switch (direction) {
+            case Phaser.UP:
+                player.body.velocity.y = -speed;
+                break;
+            case Phaser.DOWN:
+                player.body.velocity.y = speed;
+                break;
+            case Phaser.LEFT:
+                player.body.velocity.x = -speed;
+                break;
+            case Phaser.RIGHT:
+                player.body.velocity.x = speed;
+                break;
+        }
+        currentTile.x = state.math.snapToFloor(Math.floor(player.x),
+            Const.TILE_SIZE);
+        currentTile.y = state.math.snapToFloor(Math.floor(player.y),
+            Const.TILE_SIZE);
         state.physics.arcade.collide(player, collisionLayer);
+    };
+    state.render = function() {
+        state.game.debug.geom(currentTile, '#F00');
+        state.game.debug.text("Player X: " + player.x, 20, 20);
+        state.game.debug.text("Player Y: " + player.y, 20, 40);
+        state.game.debug.text("Direction: " + direction, 20, 60);
+        state.game.debug.text("Current Tile X: " + currentTile.x, 20, 80);
+        state.game.debug.text("Current Tile Y: " + currentTile.y, 20, 100);
     };
     return state;
 });
